7418e3f83d6b7e44e0c277fed8bef01aeee1d808
SparK <shishirchhetri45@gmail.com>
2022-11-03 09:58:18 +0545

    fixed async and wait

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,54 +1,72 @@
-//   Data for the meme image is fetched using an api `https://api.imgflip.com/get_memes`
+// Data for the meme image is fetched using 
+// an api "https://api.imgflip.com/get_memes"
+// replacing the memesData.js file
+
 import React from "react"
-export default function Meme(){
-    const [meme,setMeme]=React.useState({
-        topText:'',
-        bottomText:'',
-        randomImg:"https://i.imgflip.com/30b1gx.jpg"
+export default function Meme() {
+    const [meme, setMeme] = React.useState({
+        topText: '',
+        bottomText: '',
+        randomImg: "https://i.imgflip.com/30b1gx.jpg"
     })
-    const [allMemeImg,setAllMemeImg]=React.useState([])
+    const [allMemeImg, setAllMemeImg] = React.useState([])
 
-    React.useEffect(()=>{
+    React.useEffect(() => {
         fetch("https://api.imgflip.com/get_memes")
-        .then(res => res.json())
-        .then(data => setAllMemeImg(data.data.memes))
-    },[])
+            .then(res => res.json())
+            .then(data => setAllMemeImg(data.data.memes))
+        {/*
+        useEffect takes a function as its parameter. If that function
+        returns something, it needs to be a cleanup function. Otherwise,
+        it should return nothing. If we make it an async function, it
+        automatically retuns a promise instead of a function or nothing.
+        Therefore, if you want to use async operations inside of useEffect,
+        you need to define the function separately inside of the callback
+        function, as seen below:
+    async function getmemes(){
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    */}
+    }, [])
 
-    function getMemeImage(){
-        const randomNumber = Math.floor(Math.random()*allMemeImg.length)
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemeImg.length)
         const url = allMemeImg[randomNumber].url
-        setMeme(preValue=>{
-            return({
+        setMeme(preValue => {
+            return ({
                 ...preValue,
-                randomImg:url
+                randomImg: url
             })
         })
-        
+
     }
 
-    function handleChange(event){
-        const{name,value}=event.target
-        setMeme(preValue=>{
-            return({
+    function handleChange(event) {
+        const { name, value } = event.target
+        setMeme(preValue => {
+            return ({
                 ...preValue,
-                [name]:value
+                [name]: value
             })
         })
     }
-    
+
     return (
         <main>
             <div className="form">
-                <input 
-                    type="text" 
-                    placeholder="Top-text" 
+                <input
+                    type="text"
+                    placeholder="Top-text"
                     className="from-input"
                     name="topText"
                     onChange={handleChange}
-                 />
-                <input 
-                    type="text" 
-                    placeholder="Bottom-text" 
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom-text"
                     className="from-input"
                     name="bottomText"
                     onChange={handleChange}
@@ -56,11 +74,11 @@ export default function Meme(){
                 <button onClick={getMemeImage} className="form-button">Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src={meme.randomImg} className="meme--image" alt="image for meme" />
+                <img src={meme.randomImg} className="meme--image" alt="memeImg" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
-            </main>
+        </main>
     )
 }
 
